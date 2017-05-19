3fb6ad53227e3c92061aca45e655071d438fd286
Antonius W. Torode <torodean@pa.msu.edu.intranet>
2017-05-19 11:20:11 -0400

    added bin and hello world file

---
+++ bin/MIAmain.cpp
@@ -0,0 +1,8 @@
+// MIA main Program Interface and Runner.
+#include <iostream>
+
+int main(){
+	//Just a test hello World script to setup initial programming settings.
+	std::cout << "Hello World!";
+	std::cout << "I'm a C++ program";
+}
