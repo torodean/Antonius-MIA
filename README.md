2b7ceaea08a411a8469e84bf56f0778241df567a
Antonius W. Torode <torodean@pa.msu.edu.intranet>
2017-05-30 10:42:32 -0400

    added some minor setup stuff

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # Antonius-MIA
-MIA (Multiple Integrated Applications) is a program where I can store useful coding functions I've created in one place for easy accessibility.
+MIA (Multiple Integrated Applications) is a program where I can store 
+useful coding functions I've created in one place for easy accessibility.
+
+
+|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+||      - Compiling Instructions with Cygwin on Windows -        ||
+|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+
+1) Install Cygwin using hte tutorial here: http://preshing.com/20141108/how-to-install-the-latest-gcc-on-windows/
+
+2) Place the MIA directory in Cygwin64/home/username/Antonius-MIA
+
+3) cd /home/username/Antonius-MIA/bin
+
+4) compile an exe using g++ -std=c++14 MIAmain.cpp
+
+5) run using ./a.exe or rename a.exe to MIA.exe then use ./MIA.exe while 
+in the bin directory.
--- bin/MIAmain.cpp
+++ bin/MIAmain.cpp
@@ -1,8 +1,31 @@
 // MIA main Program Interface and Runner.
 #include <iostream>
+#include <string>
+	
+void splash();
+	
+//Sets the current version number of MIA.
+const std::string VERSION = "0.001";
 
+//Runs the main MIA program.
 int main(){
-	//Just a test hello World script to setup initial programming settings.
-	std::cout << "Hello World!";
-	std::cout << "I'm a C++ program";
+	splash();
+	return 0;
+}
+
+//Displays the MIA splash screen.
+void splash(){
+	std::cout << "" << std::endl;
+	std::cout << ".........................................................." << std::endl;
+	std::cout << "...       |||      |||  ||||||||||      ||||     TM    ..." << std::endl;
+	std::cout << "...       ||||    ||||      ||         ||  ||          ..." << std::endl;
+	std::cout << "...       || ||  || ||      ||        ||||||||         ..." << std::endl;
+	std::cout << "...       ||  ||||  ||      ||       ||      ||        ..." << std::endl;
+	std::cout << "...       ||   ||   ||  ||||||||||  ||        ||       ..." << std::endl;
+	std::cout << ".........................................................." << std::endl;
+	std::cout << "................... TERMINAL INTERFACE ..................." << std::endl;
+	std::cout << ".........................................................." << std::endl;
+	std::cout << "... Multiple Integrated Applications -- Version: " + VERSION + " ..." << std::endl;
+	std::cout << "..........     Programmer: Antonius Torode      .........." << std::endl;
+	std::cout << ".........................................................." << std::endl;
 }
