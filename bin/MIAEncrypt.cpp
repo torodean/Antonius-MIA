1227d94810d7c89d402ce55651a1a7ec5c3ec78a
Antonius W. Torode <torodean@pa.msu.edu.intranet>
2017-07-07 15:22:29 +0100

    Added a MIAEncrypt cpp and h file

---
+++ bin/MIAEncrypt.cpp
@@ -0,0 +1,22 @@
+//============================================================================
+// Name        : MIAEncrypt.cpp
+// Author      : Antonius Torode
+// Copyright   : This file can be used under the conditions of Antonius' 
+//				 General Purpose License (AGPL).
+// Created on  : July 7, 2017 (MIA)
+// Description : This is a d0s3 (the final version of the d0sag3 encryption version)
+//				 encryption algorithm. It Is made to encrypt any file.
+//============================================================================
+
+#include "MIAEncrypt.h"
+
+//Main constructor for the MIAEncrypt algorithm.
+MIAEncrypt::MIAEncrypt(){
+	//When Chuck Norris's code fails to compile, the computer apologizes.
+}
+
+//Main de-onstructor for the MIAEncrypt algorithm.
+MIAEncrypt::~MIAEncrypt(){
+	//Chuck Norris can stop an infinite loop just by thinking about it.
+}
+
---
+++ bin/MIAEncrypt.h
@@ -0,0 +1,22 @@
+//============================================================================
+// Name        : MIAEncrypt.h
+// Author      : Antonius Torode
+// Copyright   : This file can be used under the conditions of Antonius' 
+//				 General Purpose License (AGPL).
+// Created on  : July 7, 2017 (MIA)
+// Description : The header file corresponding to the MIAEncrypt.cpp file.
+//============================================================================
+
+#ifndef MIAENCRYPT_H
+#define MIAENCRYPT_H
+#include <string>
+
+
+class MIAEncrypt{
+	private:
+		
+	public:
+	
+};
+
+#endif // MIAENCRYPT_H
